27165bc78f15bee8cb24f4fdceea31e21dc63f4c
Techris <agadakriss@gmail.com>
2023-06-13 18:41:50 +0300

    Initial Message

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    name: "Video File1",
+    type: "video",
+    path: "/file-server/magic_-_110548 (Original).mp4",
+  },
+  {
+    id: 2,
+    name: "Audio File1",
+    type: "audio",
+    path: "/file-server/funny-tango-dramatic-music-for-vlog-video-1-minute-150834.mp3",
+  },
+  {
+    id: 3,
+    name: "Document File",
+    type: "document",
+    path: "/file-server/2. Do you wonder why bugs are good Author T. Albert-compressed.pdf",
+  },
+  {
+    id: 4,
+    name: "Image File",
+    type: "image",
+    path: "/file-server/IMG_4905.jpg",
+  },
+  {
+    id: 1,
+    name: "Video File",
+    type: "video",
+    path: "/file-server/subscribe_-_114918 (1080p).mp4",
+  },
+  {
+    id: 2,
+    name: "Audio File",
+    type: "audio",
+    path: "public/file-server/stomps-and-claps-percussion-and-rhythm-141190.mp3",
+  },
+];
